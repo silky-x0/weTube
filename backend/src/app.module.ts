7af9cf35a12d6bf94b2ef0253d4ead9bc6d9fa8c
3mindedscholar <10akhil.t@gmail.com>
2025-10-25 19:54:39 +0530

    Updated imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { CommentsModule } from './modules/comments/comments.module';
+import { VideosModule } from './modules/videos/videos.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, VideosModule, CommentsModule],
 })
 export class AppModule {}
