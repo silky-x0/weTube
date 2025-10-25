54df784b8b24b4aa3d50c28e41a6e6dfaa3f0978
3mindedscholar <10akhil.t@gmail.com>
2025-10-25 21:08:40 +0530

    db config

---
+++ backend/src/config/database.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/wetube'),
+  ],
+})
+export class DatabaseModule {}
