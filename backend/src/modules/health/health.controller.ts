e71b758fb90e1cf4e3e6edef424587945f583e24
3mindedscholar <10akhil.t@gmail.com>
2025-11-01 23:40:27 +0530

    tested Api docs via swagger

--- backend/src/modules/health/health.controller.ts
+++ backend/src/modules/health/health.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get } from "@nestjs/common";
 import { InjectConnection } from "@nestjs/mongoose";
 import { Connection } from "mongoose";
+import { ApiTags, ApiResponse } from "@nestjs/swagger";
 
+@ApiTags("Health")
 @Controller("health")
 export class HealthController {
   constructor(@InjectConnection() private readonly connection: Connection) {}
 
   @Get()
-  async check(): Promise<{ status: string, mongoState: number }> {
+  @ApiResponse({
+    status: 200,
+    description: "Health status retrieved successfully.",
+  })
+  async check(): Promise<{ status: string; mongoState: number }> {
     const dbState = this.connection.readyState;
 
     return {
