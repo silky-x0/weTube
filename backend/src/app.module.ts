4986ae5f24ae8533ffac510b4181a5a6daa05158
3mindedscholar <10akhil.t@gmail.com>
2025-11-01 23:16:36 +0530

    modules import and check

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './modules/auth/auth.module';
-import { CommentsModule } from './modules/comments/comments.module';
-import { VideosModule } from './modules/videos/videos.module';
-import { UsersModule } from './modules/users/users.module';
+import { Module, OnModuleInit } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { InjectConnection } from "@nestjs/mongoose";
+import { Connection } from "mongoose";
+import { AuthModule } from "./modules/auth/auth.module";
+import { CommentsModule } from "./modules/comments/comments.module";
+import { VideosModule } from "./modules/videos/videos.module";
+import { UsersModule } from "./modules/users/users.module";
+import { HealthModule } from "./modules/health/health.module";
 
 @Module({
-  imports: [AuthModule, UsersModule, VideosModule, CommentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env.local",
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>("MONGODB_URI"),
+        dbName: configService.get<string>("DB_NAME"),
+      }),
+      inject: [ConfigService],
+    }),
+    HealthModule,
+    AuthModule,
+    UsersModule,
+    VideosModule,
+    CommentsModule,
+  ],
 })
 export class AppModule {}
